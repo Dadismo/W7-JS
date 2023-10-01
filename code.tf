cb59f74cae2e499ed04a093e382f4052eb8cbfc4
kserge2001 <kserge2001@yahoo.fr>
2023-10-01 18:54:02 -0500

    Update code.tf

--- code.tf
+++ code.tf
@@ -138,7 +138,17 @@ resource "aws_instance" "ec2-demo" {
     instance_type = "t2.micro"
     key_name = "ec2-demo"
     subnet_id = aws_subnet.public_subnet1.id
-    user_data = file("install.sh")
+    user_data =  <<EOF
+  #!/bin/bash
+sudo yum update -y 
+sudo yum install unzip wget httpd -y  
+sudo wget https://github.com/utrains/static-resume/archive/refs/heads/main.zip
+sudo unzip main.zip
+sudo rm -rf /var/www/html/*
+sudo cp -r static-resume-main/* /var/www/html/  
+sudo systemctl start httpd
+sudo systemctl enable httpd 
+     EOF
     tags={
         Name = "Terraform instance"
         env = "Dev"
